d1c70c6751ca36e6b0b8a7563759faa52a6cd223
Matej Gjozinski <72091171+matejcho@users.noreply.github.com>
2021-08-31 13:43:17 +0200

    Update resize.py

--- resize.py
+++ resize.py
@@ -7,5 +7,5 @@ root_dir = "C:\\Users\\Matej Gjozinski\\PycharmProjects\\object_detection\\data\
 for filename in glob.iglob(root_dir + '**/*.jpg', recursive=True):
     print(filename)
     im = Image.open(filename)
-    imResize = im.resize((300, 300), Image.ANTIALIAS)
+    imResize = im.resize((416, 416), Image.ANTIALIAS)
     imResize.save(filename, 'PNG', quality=90)
